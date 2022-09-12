e8b0fe359424cb1d93c31e2c2330691570305331
mohamedfazilk <2114921@cognizant.com>
2022-09-12 14:37:45 +0530

    first commit

---
+++ carddata/src/main/java/com/packt/carddata/CarddataApplication.java
@@ -0,0 +1,13 @@
+package com.packt.carddata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarddataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarddataApplication.class, args);
+	}
+
+}
